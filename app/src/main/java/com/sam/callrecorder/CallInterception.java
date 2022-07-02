4e04b61c426940d1e94354de1d4fcf9d5af6a938
Kevinw Wall <kevin2014wally@gmail.com>
2022-07-02 17:41:59 +0200

    v2.5 Stable, Evite des instanciations inutiles

--- app/src/main/java/com/sam/callrecorder/CallInterception.java
+++ app/src/main/java/com/sam/callrecorder/CallInterception.java
@@ -25,19 +25,78 @@ import java.util.Locale;
 //TODO : ce BroadcastReceiver serait à remplacer par un service !?!?! A voir.
 public class CallInterception extends BroadcastReceiver {
     private static final String TAG = "CallInterception ";
-    private TelecomManager telecomManager;
-    private Context myContext;
-    private PhoneAccountHandle phoneAccountHandle=null;
+
+    //private static Context myContext;
+
+    private static TelephonyManager telephonyManager=null;
+    private static MyPhoneStateListener phoneListener=null;
+
+    private static TelecomManager telecomManager=null;
+    private static PhoneAccountHandle phoneAccountHandle=null;
+
+    private final String   ACTION_PHONE_STATE  = "android.intent.action.PHONE_STATE";
+
+    static int nbFois = 0;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        //Gestion de la "Telephony Manager" pour recevoir les appels entrants
-        TelephonyManager tmgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        MyPhoneStateListener phoneListener = new MyPhoneStateListener();
-        tmgr.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+        nbFois++;
+        Log.i(TAG, "onReceive(), IN nb fois est " + nbFois);
 
-        myContext = context;
+        //Gestion de la "Telephony Manager" pour recevoir les appels entrants
+        if (telephonyManager == null) {
+            telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            phoneListener = new MyPhoneStateListener();
+            telephonyManager.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+            Log.i(TAG, "onReceive(), telephonyManager vient d'instancier un phoneListener " + telephonyManager.toString());
+        } else {
+            //Log.i(TAG, "onReceive(), telephonyManager a déjà instancié un phoneListener Sim nb est " + telephonyManager.getSimSerialNumber()); //permission READ_PRIVILEGED_PHONE_STATE is only granted to system apps
+            //Log.i(TAG, "onReceive(), telephonyManager a déjà instancié un phoneListener Line1Number est " + telephonyManager.getLine1Number());
+            //Log.i(TAG, "onReceive(), telephonyManager a déjà instancié un phoneListener IMEI est " + telephonyManager.getImei());
+            Log.i(TAG, "onReceive(), telephonyManager a déjà instancié un phoneListener, telephonyManager est " + telephonyManager.toString());
+        }
+
+        //Init myContext pour être utilisé
+        //myContext = context;
+
+        Log.i(TAG, "onReceive(), intent.getAction()  ? "+ intent.getAction());
+        Log.i(TAG, "onReceive(), intent.getDataString()  ? "+ intent.getDataString());
+
+        //Tests pour voir le contenu de l'intent
+        if (intent.getAction().equals(ACTION_PHONE_STATE))
+        {
+            Bundle bundle = intent.getExtras();
+
+            Log.i(TAG, "onReceive(), ACTION_PHONE_STATE : intent extras est " + intent.getExtras().toString());
+
+//            if (bundle != null)
+//            {
+//                Object[] pdus = (Object[]) bundle.get("pdus");
+//
+//                if(pdus != null) {
+//                    Log.i(TAG, "onReceive(), ACTION_PHONE_STATE : pdus length est " + pdus.length);
+//                } else {
+//                    Log.i(TAG, "onReceive(), ACTION_PHONE_STATE : pdus is null");
+//                }
+//
+//
+//                /*final SmsMessage[] messages = new SmsMessage[pdus.length];
+//                for (int i = 0; i < pdus.length; i++)  {
+//                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);  }
+//
+//                if (messages.length > -1)
+//                {
+//                    final String messageBody = messages[0].getMessageBody();
+//                    final String phoneNumber = messages[0].getDisplayOriginatingAddress();
+//
+//                    Toast.makeText(context, "Expediteur : " + phoneNumber, Toast.LENGTH_LONG).show();
+//                    Toast.makeText(context, "Message : " + messageBody, Toast.LENGTH_LONG).show();
+//                }*/
+//            } else {
+//                Log.i(TAG, "onReceive(), bundle is null");
+//            }
+        }
 
         //220627: Code mis en commentaire mais qui peut servir pour l'enregistrement des appels
         //Intent serviceIntent = new Intent(context, CallRecorder.class);
@@ -46,19 +105,21 @@ public class CallInterception extends BroadcastReceiver {
         //context.startService(serviceIntent);
 
         //Gestion du "Telecom Manager" et d'un gestionnaire de comptes d'appels téléphoniques
-        telecomManager = (TelecomManager) context.getSystemService(Context.TELECOM_SERVICE);
+        if (telecomManager == null) {
+            Log.i(TAG, "onReceive(), telecomManager encore à null, phoneAccountHandle va être instancié ...");
+            telecomManager = (TelecomManager) context.getSystemService(Context.TELECOM_SERVICE);
 
-        if (phoneAccountHandle == null) {
-            Log.i(TAG, "onReceive(), phoneAccountHandle.getId() est null");
+        //if (phoneAccountHandle == null) {
+
 
             phoneAccountHandle = new PhoneAccountHandle(
                     //new ComponentName(this.getApplicationContext(), MyConnectionService.class),
                     //new ComponentName(MainActivity.this, MyConnectionService.class),
-                    new ComponentName(myContext, MyConnectionService.class),
+                    new ComponentName(context, MyConnectionService.class),
                     //new    ComponentName(contextLocal.getApplicationContext().getPackageName(),MyConnectionService.class.getName()),
                     "examplee");
 
-            Log.i(TAG, "onReceive(), phoneAccountHandle.getId() est " + phoneAccountHandle.getId());
+            Log.i(TAG, "onReceive(), phoneAccountHandle.getId() est maintenant \"" + phoneAccountHandle.getId() + "\" \n (Vérifier si enabled!!)");
 
             //TODO : vérifier les permissions de PhoneAccount
             //PhoneAccount phoneAccount = PhoneAccount.builder(phoneAccountHandle, "examplee").setCapabilities(PhoneAccount.CAPABILITY_CALL_PROVIDER).build();
@@ -71,13 +132,12 @@ public class CallInterception extends BroadcastReceiver {
 
             telecomManager.registerPhoneAccount(phoneAccount);
         } else {
+            Log.i(TAG, "onReceive(), telecomManager est déjà instancié " + telecomManager.toString());
             Log.i(TAG, "onReceive(), phoneAccountHandle.getId() est " + phoneAccountHandle.getId());
         }
     }
 
     private class MyPhoneStateListener extends PhoneStateListener {
-
-        //private Context contextLocal;
 
         @SuppressLint("ObsoleteSdkInt")
         @RequiresApi(api = Build.VERSION_CODES.O) //Android Oreo 8.0 August 2017
@@ -85,7 +145,7 @@ public class CallInterception extends BroadcastReceiver {
         public void onCallStateChanged(int state, String incomingNumber) {
             super.onCallStateChanged(state, incomingNumber);
 
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+            /*SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
             String time =  dateFormat.format(new Date()) ;
 
             Log.i(TAG, "onCallStateChanged(), time is "+ time);
@@ -96,21 +156,22 @@ public class CallInterception extends BroadcastReceiver {
                 Log.i(TAG, "onCallStateChanged(), directory created is "+ sampleDir.getAbsolutePath());
             }
             Log.i(TAG, "onCallStateChanged(), directory created is "+ sampleDir.getPath());
+            */
 
 
             switch (state) {
                 case TelephonyManager.CALL_STATE_IDLE:
                     // CALL_STATE_IDLE;
-                    Log.i(TAG, "onCallStateChanged: IDLE " + incomingNumber);
+                    Log.i(TAG, "onCallStateChanged() CALL_STATE_IDLE " + incomingNumber);
                     break;
                 case TelephonyManager.CALL_STATE_OFFHOOK:
                     // CALL_STATE_OFFHOOK;
-                    Log.i(TAG, "onCallStateChanged: OFF HOOK " + incomingNumber);
+                    Log.i(TAG, "onCallStateChanged() CALL_STATE_OFFHOOK " + incomingNumber);
                     break;
                 case TelephonyManager.CALL_STATE_RINGING:
                     // CALL_STATE_RINGING
                     //https://stackoverflow.com/questions/43027292/addincomingcall-in-android-telecommanager-not-doing-anything
-                    Log.i(TAG, "onCallStateChanged: RINGING " + incomingNumber);
+                    Log.i(TAG, "onCallStateChanged() CALL_STATE_RINGING " + incomingNumber);
 
                     if (incomingNumber != null) {
                         //tm = (TelecomManager) getSystemService(Context.TELECOM_SERVICE);
@@ -159,18 +220,21 @@ public class CallInterception extends BroadcastReceiver {
                             Bundle extras = new Bundle();
 
                             //Uri uri = Uri.fromParts(PhoneAccount.SCHEME_TEL, mNumber.getText().toString(), null);
-                            Log.i(TAG," on onCallStateChanged(), tm.getLine1Number(phoneAccountHandle) is " + telecomManager.getLine1Number(phoneAccountHandle));
+                            //Log.i(TAG," on onCallStateChanged(), tm.getLine1Number(phoneAccountHandle) is " + telecomManager.getLine1Number(phoneAccountHandle));
                             //Uri uri = Uri.fromParts(PhoneAccount.SCHEME_TEL, tm.getLine1Number(phoneAccountHandle), null);
 
                             Uri uri = Uri.fromParts(PhoneAccount.SCHEME_TEL, incomingNumber.toString(), null);
-                            Log.i(TAG, "URI est " + uri.toString());
+                            Log.i(TAG, "URI du PhoneAccount.SCHEME_TEL est [" + uri.toString() + "]");
+                            //Uri uri2 = Uri.fromParts(PhoneAccount.SCHEME_VOICEMAIL, incomingNumber.toString(), null);
+                            //Log.i(TAG, "URI du PhoneAccount.SCHEME_VOICEMAIL est [" + uri2.toString() + "]");
 
                             extras.putParcelable(TelecomManager.EXTRA_INCOMING_CALL_ADDRESS, uri);
                             extras.putParcelable(TelecomManager.EXTRA_PHONE_ACCOUNT_HANDLE, phoneAccountHandle);
 
                             //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                             //    if(tm.isIncomingCallPermitted(phoneAccountHandle)) {
-                            telecomManager.addNewIncomingCall(phoneAccountHandle, extras);
+
+                            telecomManager.addNewIncomingCall(phoneAccountHandle, extras); //Nécessite que  "This PhoneAccountHandle is not enabled for this user!" sinon message error s'afiche.
                             Log.i(TAG, "Call system via tm.addNewIncomingCall(), done");
                             //   } //requires permission MANAGE_OWN_CALLS
                             //}
